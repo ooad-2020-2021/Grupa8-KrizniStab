4c5067399f962a353c3d5721d856b6f382ca2d38
shadzijusu1 <shadzijusu1@etf.unsa.ba>
2021-05-03 17:57:46 +0200

    Inicijalni projekat

---
+++ Implementacija/CovidX/CovidX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CovidX.Startup))]
+namespace CovidX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
